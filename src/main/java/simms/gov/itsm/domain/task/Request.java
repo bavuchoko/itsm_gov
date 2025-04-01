c42c44834812f0642dbf71b91421bc6deef5b447
pjs <bavuchoko@naver.com>
2025-04-01 13:31:56 +0900

    [refactor] TaskManager를 상혹하는 Task의 구현체에게 TaskManager의 필드를 접근 할 수 있도록 변경, 필드 추가함

--- src/main/java/simms/gov/itsm/domain/task/Request.java
+++ src/main/java/simms/gov/itsm/domain/task/Request.java
@@ -1,18 +1,26 @@
 package simms.gov.itsm.domain.task;
 
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
-import lombok.*;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
 import simms.gov.itsm.domain.task.sub.Accept;
+import simms.gov.itsm.domain.task.vo.ReqeustDate;
 
 import java.util.Arrays;
 
 @Entity
 @Getter
-@Builder
+@SuperBuilder
+@DiscriminatorValue("Request")
 public class Request extends TaskManager implements Task {
 
+    @Embedded
+    private ReqeustDate requestDateInfo;
 
+    private String etc;
 
     public Request(){
         super(Arrays.asList(new Accept()));
