093f0f1f281fa59f9e207fafeda15d68ca9dce80
pjs <bavuchoko@naver.com>
2025-04-01 13:33:39 +0900

    [refactor] Task의 구현체들의 생성을 관리한 Factory클래스 추가

--- src/main/java/simms/gov/itsm/domain/task/TaskFactory.java
+++ src/main/java/simms/gov/itsm/domain/task/TaskFactory.java
@@ -1,4 +1,11 @@
 package simms.gov.itsm.domain.task;
 
 public class TaskFactory {
+
+    public Request createRequest(TaskManager taskManager) {
+        return Request.builder()
+                .userInfo(taskManager.userInfo)
+                .dateInfo(taskManager.dateInfo)
+                .build();
+    }
 }
