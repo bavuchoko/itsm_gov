e09469d552d4459cf42fb910bbb830c2b121fd09
pjs <bavuchoko@naver.com>
2025-04-09 20:34:44 +0900

    [feat] 요청 팩토리 메서드 추가.
    등록일과 등록자는 필수, 등록시 접수단계 생성

--- src/main/java/simms/gov/itsm/domain/task/TaskFactory.java
+++ src/main/java/simms/gov/itsm/domain/task/TaskFactory.java
@@ -1,11 +1,29 @@
 package simms.gov.itsm.domain.task;
 
+import org.springframework.stereotype.Component;
+import simms.gov.itsm.domain.task.dto.TaskRequestDto;
+import simms.gov.itsm.domain.task.mapper.TaskMapper;
+import simms.gov.itsm.domain.task.sub.Accept;
+
+import java.util.List;
+
+@Component
 public class TaskFactory {
 
-    public Request createRequest(TaskManager taskManager) {
-        return Request.builder()
-                .userInfo(taskManager.userInfo)
-                .dateInfo(taskManager.dateInfo)
-                .build();
+    public Claim claimOf(TaskRequestDto taskManager) {
+
+
+        if(taskManager.getCreateDate()==null) throw new IllegalArgumentException("TaskRequestDto must have createDate");
+        if(taskManager.getCreateUser()==null) throw new IllegalArgumentException("TaskRequestDto must have createUser");
+
+        Claim claim = TaskMapper.INSTANCE.toClaim(taskManager);
+
+        Accept accept = new Accept();
+        accept.setTask(claim);
+
+        claim.setProcedures(List.of(accept));
+
+        return claim;
     }
+
 }
