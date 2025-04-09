6641b187d1e60a4de5a19eebf62df2d401379ae9
pjs <bavuchoko@naver.com>
2025-04-09 20:33:54 +0900

    [feat] role 매핑 없는 유저 매퍼 추가

--- src/main/java/simms/gov/itsm/domain/user/mapper/AccountMapper.java
+++ src/main/java/simms/gov/itsm/domain/user/mapper/AccountMapper.java
@@ -2,6 +2,7 @@ package simms.gov.itsm.domain.user.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 import simms.gov.itsm.domain.user.dto.AccountRequestDto;
@@ -16,9 +17,18 @@ public interface AccountMapper {
     @Mapping(target = "name", source = "accountInfo.name")
     @Mapping(target = "userName", source = "accountInfo.userName")
     @Mapping(target = "email", source = "accountInfo.email")
-    @Mapping(target = "password", source = "password")
+    @Mapping(target = "password", ignore = true)
     @Mapping(target = "roles", source = "roles")
     AccountResponseDto entityToResponse(Account account);
+
+
+    @Named("toUserResponseNoRole")
+    @Mapping(target = "name", source = "accountInfo.name")
+    @Mapping(target = "userName", source = "accountInfo.userName")
+    @Mapping(target = "email", source = "accountInfo.email")
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "roles", ignore  = true)
+    AccountResponseDto toUserResponseNoRole(Account account);
 
     @Mapping(target = "accountInfo", expression = "java(new simms.gov.itsm.domain.user.vo.AccountInfo(account.getName(), account.getUserName(), account.getEmail()))")
     @Mapping(target = "password", source = "password")
