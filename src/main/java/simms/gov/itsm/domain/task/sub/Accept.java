1b3aef5ee6f5851e56ad75af3c0e2e6dba06071f
pjs <bavuchoko@naver.com>
2025-04-09 20:32:45 +0900

    [feat] task저장시 같이 저장될 수 있도록 서브작업에 부모 작업을 set해줌

--- src/main/java/simms/gov/itsm/domain/task/sub/Accept.java
+++ src/main/java/simms/gov/itsm/domain/task/sub/Accept.java
@@ -1,8 +1,10 @@
 package simms.gov.itsm.domain.task.sub;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import simms.gov.itsm.domain.task.TaskManager;
 import simms.gov.itsm.domain.user.entity.Account;
 
 
@@ -14,7 +16,9 @@ public class Accept extends SubTaskManager implements SubTask{
     @JoinColumn(name = "approval_user")
     private Account approvalUser;
 
-
+    public void setTask(TaskManager task) {
+        super.task = task;
+    }
 
     @Override
     public void done(Contentable item) {
